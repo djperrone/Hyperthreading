9a22a7ed96b6d48b28607e2c5b2c8538596cd7a3
djperrone <60362379+djperrone@users.noreply.github.com>
2021-04-17 23:02:36 -0400

    final code with thread pool

--- test/test/src/main.cpp
+++ test/test/src/main.cpp
@@ -7,7 +7,9 @@
 #include "ctpl_stl.h"
 
 
+
 void add(int id, int count);
+void addFloat(int id, int count);
 void runThreadTest(int numTasks, int numThreads);
 
 std::mutex cout_mutex;
@@ -16,14 +18,25 @@ int main()
 {
 	int numTasks = 16;
 	int numThreads = 4;
-	Timer timer("no_hyperthreading_test3.csv");
+
+	if (numThreads > 4)
+	{
+		std::cout << "Too many threads!\n";
+		return 0;
+	}
+		
+
+	Timer timer("no_hyperthreading_test6.csv");
 
 	timer.WriteHeader();
+
+	
 
 	for (int th = 1; th <= numThreads; th*=2)
 	{
 		timer.setnumThreads(th);
-		for (int task = 1; task <= numTasks ;task++)
+	//int th = 1;
+		for (int task = 2; task <= numTasks ;task++)
 		{
 			timer.setNumTasks(task);
 			timer.Start();
@@ -32,52 +45,11 @@ int main()
 			timer.Stop();
 			timer.Calc();
 			timer.WriteCSV();
+			std::cout << "\n";
 			std::cout <<th<<" threads, "<<task<<" tasks: "<< timer.GetDuration() << " seconds" << std::endl;
 			std::cout << "\n";
 		}
 	}
-
-
-
-
-
-
-
-	/*int numTasks = 4;
-	int numThreads = 4;
-
-	Timer timer;
-
-	timer.Start();
-	add(1,1); 
-	timer.Stop();
-	timer.Calc();
-
-	std::cout << timer.GetDuration() << " seconds" << std::endl;
-
-	timer.Start();
-	runThreadTest(1, 1);
-	timer.Stop();
-	timer.Calc();
-	std::cout << timer.GetDuration() << " seconds" << std::endl;
-
-
-	timer.Start();
-
-	runThreadTest(numTasks, 1);
-
-	timer.Stop();
-	timer.Calc();
-	
-	std::cout << timer.GetDuration() << " seconds" << std::endl;
-
-	timer.Start();
-
-	runThreadTest(numTasks, numTasks);
-
-	timer.Stop();
-	timer.Calc();
-	std::cout << timer.GetDuration() <<" seconds"<< std::endl;*/
 }
 
 void runThreadTest(int numTasks, int numThreads)
@@ -87,6 +59,7 @@ void runThreadTest(int numTasks, int numThreads)
 	for (int i = 0; i < numTasks; i++)
 	{
 		pool.push(add, i+1);
+		//std::cout<<"id "<<pool.get_thread(0).get_id()<<std::endl;
 	}
 }
 
@@ -98,13 +71,34 @@ void add(int id, int count)
 	cout_lock.unlock();
 
 	int sum = 0;
+	//float sumf = 0.0f;
 	for (int i = 0; i < 600000000; i++)
 	{
 		sum++;
+		//sumf += 1.01f;
+	}	
+
+	cout_lock.lock();
+	std::cout << "Finished task " << count << ", int sum is " << sum << std::endl;
+	//std::cout << "Finished task " << count << ", int sum is " << sum <<"float sum is "<<sumf<< std::endl;
+	cout_lock.unlock();
+}
+
+void addFloat(int id, int count)
+{
+	std::unique_lock<std::mutex> cout_lock(cout_mutex);
+
+	std::cout << "started task " << count << std::endl;
+	cout_lock.unlock();
+
+	float sum = 0.0f;
+	for (float i = 0; i < 300000000; i++)
+	{
+		sum+=1.0f;
 	}
 
 	cout_lock.lock();
-	std::cout << "Finished task " << count << ", sum is " << sum << std::endl;
+	std::cout << "Finished task " << count << ", sum is " << sum << std::endl<<std::endl;
 	cout_lock.unlock();
 }
 
